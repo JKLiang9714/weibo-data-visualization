7281a1540ff65e2c4d8ab89d70734b9b7e8a7d4f
maoyiwei <jsmyw95@qq.com>
2018-12-30 18:30:57 +0800

    feature: add table for bloggers

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,13 +1,44 @@
+import { Table } from "antd";
+import { connect } from "dva";
+import Link from 'umi/link'
 import styles from './index.css';
 
-export default function() {
+
+const mapStateToProps = (state) => ({
+  bloggers: state.blogger.list
+})
+
+const columns = [{
+  title: '姓名',
+  dataIndex: 'name',
+  render: text => <Link to="/">
+    {text}
+  </Link>
+}, {
+  title: '性别',
+  dataIndex: 'sex',
+}, {
+  title: '出生地',
+  dataIndex: 'birthplace',
+}, {
+  title: '关注',
+  dataIndex: 'following',
+  sorter: (a, b) => a.following - b.following,
+}, {
+  title: '粉丝',
+  dataIndex: 'followers',
+  sorter: (a, b) => a.followers - b.followers,
+}];
+
+function Index(props) {
   return (
     <div className={styles.normal}>
-      <div className={styles.welcome} />
-      <ul className={styles.list}>
-        <li>To get started, edit <code>src/pages/index.js</code> and save to reload.</li>
-        <li><a href="https://umijs.org/guide/getting-started.html">Getting Started</a></li>
-      </ul>
+      <Table
+        columns={columns}
+        dataSource={props.bloggers}
+      />
     </div>
   );
 }
+
+export default connect(mapStateToProps)(Index)
