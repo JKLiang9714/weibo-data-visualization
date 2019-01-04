d2304809566a66cf2b51e2622d0c3a31415feead
maoyiwei <jsmyw95@qq.com>
2019-01-04 16:44:04 +0800

    fix: heat map

--- frontend/src/pages/HeatMap/index.js
+++ frontend/src/pages/HeatMap/index.js
@@ -9,7 +9,6 @@ const mapStateToProps = (state) => ({
 
 const getOption = (data) => {
   let map_data = [];
-  let draw_map_data = [];
   let max_value = 0;
   for (var i = 0; i < data.length; i++) {
     map_data.push({
@@ -18,7 +17,8 @@ const getOption = (data) => {
     });
     max_value = Math.max(max_value, data[i].value);
   }
-  while (max_value % 1000 != 0) max_value++;
+  while (max_value % 1000 !== 0) max_value++;
+
   return {
     title: {
       text: '博主地域热力分布',
@@ -26,18 +26,25 @@ const getOption = (data) => {
     },
     tooltip: {
       trigger: 'item',
-      formatter: function(val){
-        var area_content =  '地区：' + val.name + '</br>人数：' + val.value;
-        return area_content.split("-").join("\n");
+      formatter: function (val) {
+        if (val.data) {
+          var area_content = '地区：' + val.name + '</br>人数：' + val.data.originValue;
+          return area_content.split("-").join("\n");
+        }
+        return ""
       },
     },
     visualMap: {
       min: 0,
-      max: max_value,
+      max: Math.log(max_value),
       left: 'left',
       top: 'bottom',
       text: ['多', '少'],           // 文本，默认为数值文本
       calculable: true,
+      formatter: function (value) {
+        if (value === 0) return 0
+        return Math.floor(Math.exp(value))
+      }
     },
     series: [
       {
@@ -51,7 +58,11 @@ const getOption = (data) => {
             show: true,
           },
         },
-        data: map_data,
+        data: map_data.map(v => ({
+          ...v,
+          originValue: v.value,
+          value: Math.log(v.value)
+        })),
       },
     ],
   };
